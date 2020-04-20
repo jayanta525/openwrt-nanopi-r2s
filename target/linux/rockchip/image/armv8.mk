81e9acf3b0799126733ca3a6c83a98679b618ef9
Lucian Cristian <lucian.cristian@gmail.com>
2020-04-21 00:13:00 +0300

    rk3328: add Pine64 Rock64 board
    also update ATF rockchip to final release
    
    Signed-off-by: Lucian Cristian <lucian.cristian@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -4,6 +4,17 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+
+define Device/pine64_rock64
+  DEVICE_VENDOR := Pine64
+  DEVICE_MODEL := Rock64
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := rock64-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES += kmod-drm-rockchip kmod-drm-rockchip-hdmi-sound
+endef
+TARGET_DEVICES += pine64_rock64
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
