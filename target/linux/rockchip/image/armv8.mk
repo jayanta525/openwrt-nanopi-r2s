0bcd5774075f1bd4f168b611a6d01f85be0da2e8
jayanta525 <jayanta.gogoi525@gmail.com>
2020-05-12 07:01:24 +0000

    rk3328: add initial support for nanopi-r2s -inital openwrt support(might be unstable) -package and luci included -build using azure pipeline -using kernel from friendlyarm/rockchip/kernel -using uboot patches from armbian/build
    Signed-off-by: Jayantajit Gogoi <jayanta.gogoi525@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -5,6 +5,16 @@
 # See /LICENSE for more information.
 #
 
+define Device/friendlyelec_nanopi-r2-rev00
+  DEVICE_VENDOR := FriendlyELEC
+  DEVICE_MODEL := NanoPi R2S
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := nanopi-r2-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152
+endef
+TARGET_DEVICES += friendlyelec_nanopi-r2-rev00
+
 define Device/pine64_rock64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := Rock64
