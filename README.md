2b0f8df06ddf847c78b9bc88a3f328ed1b71c509
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-24 07:22:12 +0000

    circleci: start building
    - new cache

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 NanoPi R2S
+-update 2
+
+check rk3328 branch for instructions
+or forum here: https://forum.openwrt.org/t/nanopi-r2s-is-a-great-openwrt-device/65374/79?u=jayanta525
--- scripts/adjust-feeds.sh
+++ scripts/adjust-feeds.sh
@@ -1,2 +1,12 @@
+# irqbalance
 sed -i 's/0/1/g' feeds/packages/utils/irqbalance/files/irqbalance.config
+#luci architecture
 sed -i "s,boardinfo.system,'ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+# Maximum connection
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+#irq_optimize
+mkdir package/base-files/files/usr/bin
+wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize.sh -O package/base-files/files/usr/bin/irq_optimize.sh
+chmod +x package/base-files/files/usr/bin/irq_optimize.sh
+wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize -O package/base-files/files/etc/init.d/irq_optimize
+chmod +x package/base-files/files/etc/init.d/irq_optimize
